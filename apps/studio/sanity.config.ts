722985bba72a0bb810af5ee35368c02dbcab1ac3
Aaron Emery <aaron.emery@sanity.io>
2024-09-07 12:54:42 -0700

    feat: adding document resolution to presentation mode

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -2,10 +2,21 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import {presentationTool} from 'sanity/presentation'
+import {
+  presentationTool,
+  DocumentLocation,
+  defineDocuments,
+  defineLocations,
+} from 'sanity/presentation'
+import {resolveHref} from './lib/utils'
 
 const previewUrl = 'http://localhost:3000/blog'
 const embeddedOrigin = 'http://localhost:3000'
+
+const homeLocation = {
+  title: 'Home',
+  href: '/',
+} satisfies DocumentLocation
 
 export default defineConfig({
   name: 'default',
@@ -18,11 +29,44 @@ export default defineConfig({
     structureTool(),
     visionTool(),
     presentationTool({
+      resolve: {
+        mainDocuments: defineDocuments([
+          {
+            route: '/posts/:slug',
+            filter: `type == "post" && slug.current == $slug`,
+          },
+        ]),
+        locations: {
+          settings: defineLocations({
+            locations: [homeLocation],
+            message: 'This document is used on all pages',
+            tone: 'caution',
+          }),
+          post: defineLocations({
+            select: {
+              title: 'title',
+              slug: 'slug.current',
+            },
+            resolve: (doc) => ({
+              locations: [
+                {
+                  title: doc?.title || 'Untitled',
+                  href: resolveHref('post', doc?.slug)!,
+                },
+                homeLocation,
+              ],
+            }),
+          }),
+        },
+      },
       previewUrl: {
         origin: previewUrl,
 
         previewMode: {
-          enable: 'http://localhost:3000/api/draft-mode/enable',
+          enable:
+            process.env.NODE_ENV === 'development'
+              ? 'http://localhost:3000/api/draft-mode/enable'
+              : 'api/draft-mode/enable',
         },
       },
     }),
